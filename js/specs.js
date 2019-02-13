3596e2e3933f5035e0e2b9f5e7e1bd4e5db67fc4
marrybaz <47028765+marrybaz@users.noreply.github.com>
2019-02-13 17:52:21 +0100

    Add files via upload

---
+++ js/specs.js
@@ -0,0 +1,10 @@
+describe('Testiranje programa za knjizaru', function () {
+    
+    it("treba da sadrzi listu knjiga", function () {
+        expect(knjige).toBeDefined();
+    })
+
+    
+
+    
+})
